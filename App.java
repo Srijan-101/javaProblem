0e416a0468a3b9eb554458d958c8c195171f622e
Srijan <59310528+Srijan-101@users.noreply.github.com>
2020-12-12 16:33:39 +0545

    Update App.java

--- App.java
+++ App.java
@@ -4,24 +4,19 @@ import java.util.*;
 
 class App {
     public static void main(String[] args) {
-        String given = "a12bcbc6789a";
+        String given = "6-3sd1a221a12bc78te8373";
         int temp = 0, k = 0;
-        int arr[] = new int[4];
-
-
-        //read string and sum consecutive numerical value and push it to array
-        // for example if string is a12bcbc6789a
-        // output array should be [3,30]
+        ArrayList<Integer> arrr = new ArrayList<Integer>();
 
         for(int i = 0 ; i < given.length() ; i++){
-            if (!(Character.isAlphabetic(given.charAt(i)))) {
+            if (Character.isDigit(given.charAt(i))) {
                  temp =  Character.getNumericValue(given.charAt(i));
-                 arr[k] = temp;
+                 arrr.add(k,temp);
                    for(int j = i + 1 ; j < given.length() ; j++){
-                      if (!(Character.isAlphabetic(given.charAt(j)))) { 
-                           temp = temp + Character.getNumericValue(given.charAt(j));
-                           arr[k] = temp;
-                           i++;
+                      if (Character.isDigit(given.charAt(j))) { 
+                          temp = temp + Character.getNumericValue(given.charAt(j));
+                          arrr.set(k,temp);
+                          i++;
                        }else{
                            k++;
                            break;
@@ -29,8 +24,12 @@ class App {
                    }
             }
         }
-        for(int j : arr){
-            System.out.println(j);
+        int[] arr = new int[arrr.size()];
+        for(int i = 0 ; i < arrr.size();i++){
+             arr[i] = arrr.get(i);
         }
+       for(int i : arr){
+           System.out.println(i);
+       }
     }
 }

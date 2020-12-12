b4b3be2ae7987d0a283c94f7c59d768a369b52c8
Srijan <59310528+Srijan-101@users.noreply.github.com>
2020-12-12 16:34:03 +0545

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,10 @@
 import java.io.Console;
 import java.util.*;
 
+//read string and sum consecutive numerical value and push it to array 
+// for example if string is a12bcbc6789a 
+// output array should be [3,30]
+
 
 class App {
     public static void main(String[] args) {
